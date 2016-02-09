eb96a5f3c6d0b72cf31d2105e0d4dcaf8d743e5d
Jason James <jaso66@gmail.com>
2016-02-09 12:10:40 -0500

    Incorporated KB's files to project Created base.html template Created login.html template

---
+++ applab/apps/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from apps import views
+
+urlpatterns = [
+    url(r'^$', views.hello_world, name='hello_world'),
+    url(r'^login/$', views.login, name='login'),
+]
