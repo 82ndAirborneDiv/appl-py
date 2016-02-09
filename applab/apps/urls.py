f9bc2d3771fb263613285fa39165f651f11c5ff8
missilen <tnmissilen@gmail.com>
2016-02-09 14:57:32 -0500

    added base applab,  applab home , iOS , android

--- applab/apps/urls.py
+++ applab/apps/urls.py
@@ -4,4 +4,7 @@ from apps import views
 urlpatterns = [
     url(r'^$', views.hello_world, name='hello_world'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^applab/$', views.home, name='applab_home'),
+    url(r'^applab/ios/', views.ios, name='applab_ios'),
+    url(r'^applab/android/', views.android, name='applab_android')
 ]
--- applab/apps/views.py
+++ applab/apps/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+
 def hello_world(request):
     return render(request, 'applab/hello_world.html')
 
+
 def login(request):
     return render(request, 'applab/login.html')
+
+
+def home(request):
+    return render(request, 'applab/home.html')
+
+
+def ios(request):
+    return render(request, 'applab/ios.html')
+
+
+def android(request):
+    return render(request, 'applab/android.html')
