d184e17796f707671b029e7fecb1fe1622b52f43
missilen <tnmissilen@gmail.com>
2016-02-24 01:09:47 -0500

    wired up most of the field.  download buttons pending

--- applab/apps/views.py
+++ applab/apps/views.py
@@ -42,21 +42,24 @@ def android_page(request):
 def app_page(request,project_title):
     groupSize = 4
     platform = project_title.split('-')[0]
-    app_title = ' '.join(project_title.split('-')[1:])
-    appDetail = ProjectOverview.objects.filter(project__in=Project.objects.filter(is_archived=False, title = app_title).order_by('title'))
-    screenshots = ProjectOverviewScreenshot.objects.filter(project_overview = appDetail)
-    screenshotGroups = [screenshots[i:i+groupSize] for i in range(0, len(screenshots), groupSize)]
-    # appDetail.screenshotGroups = screenshotGroups
-    # appDetail.title = app_title
-    # appDetail.platform = platform
-    # app_version = []
-    # appRelease = {
-    #     'major' : app_version[0],
-    #     'minor' : app_version[1],
-    #     'point' : app_version[2],
-    #     'version': app_version[3],
-    #
-    # }
+    appTitle = ' '.join(project_title.split('-')[1:-4])
+    appRelease = project_title.rsplit('-')[-4:]
+
+    appOverview = ProjectOverview.objects.filter(project__in=Project.objects.filter(is_archived=False, title = appTitle))[0]
+    screenshots = ProjectOverviewScreenshot.objects.filter(project_overview = appOverview.project_id)
+    if platform == 'ios':
+        platformReleaseDetail = IosRelease.objects.select_related('ios_project__project_overview').filter(ios_project__project_overview__project_id=appOverview.project_id,major_version=appRelease[0],minor_version=appRelease[1],point_version=appRelease[2],build_version=appRelease[3])[0]
+    elif platform == 'android':
+        platformReleaseDetail = AndroidRelease.objects.select_related('android_project__project_overview').filter(android_project__project_overview__project_id=appOverview.project_id,major_version=appRelease[0],minor_version=appRelease[1],point_version=appRelease[2],build_version=appRelease[3])[0]
+    appDetail = {
+        'overview' : appOverview,
+        'releaseDetail': platformReleaseDetail,
+        'screenshotGroups': [screenshots[i:i + groupSize] for i in range(0, len(screenshots), groupSize)],
+        'title': appTitle,
+        'platform': platform,
+        'releaseVersion': '.'.join(appRelease)
+    }
+    #appDetail.appRelease = '{0}.{1}.{2}.{3}'.format(appDetail.major_version,appDetail.minor_version,appDetail.point_version,appDetail.build_version)
     return render(request,'applab/app-page.html/',{
         'appDetail' : appDetail,
     })
