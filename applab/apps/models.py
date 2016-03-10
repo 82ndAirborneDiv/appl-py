94fea763c8d987e619064b79abc92de7b1437fe3
Greg Ledbetter <gsledbetter@gmail.com>
2016-03-10 07:56:01 -0500

    Added major and minor version numbers for project overview. Add path methods for sreenshots, icons, binaries that keeps project assets organized in media directory.

--- applab/apps/models.py
+++ applab/apps/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils.timezone import localtime
 from django.utils.html import mark_safe
+from applab.settings import MEDIA_ROOT
+
+import os.path
 
 
 class Project(models.Model):
@@ -13,15 +16,22 @@ class Project(models.Model):
         return self.title
 
 
+def overview_icon_upload_path(instance, filename):
+    date_created = str(localtime(instance.date_published).strftime('%Y-%m-%d_%I-%M_%p'))
+    return os.path.join(MEDIA_ROOT, instance.project.project_code_name, date_created, "icons", filename)
+
+
 class ProjectOverview(models.Model):
     """ A ProjectOverview object belongs to a Project and contains text description, an icon, and has multiple screenshots
     as it is a foreign key to the ProjectScreenshot object. The attributes of this object may evolve over time and are
     separated from the Project object
     """
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    description = models.TextField()
-    icon = models.ImageField()
+    major_version = models.PositiveSmallIntegerField()
+    minor_version = models.PositiveSmallIntegerField()
     date_published = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    icon = models.ImageField(upload_to=overview_icon_upload_path)
     source_code_link = models.URLField()
 
     def icon_image(self):
@@ -29,14 +39,18 @@ class ProjectOverview(models.Model):
     icon_image.allow_tags = True
 
     def __str__(self):
-        return str(localtime(self.date_published).strftime('%Y-%m-%d %I:%M %p'))
+        return '%d.%d' % (self.major_version, self.minor_version)
+
+def overview_screenshot_upload_path(instance, filename):
+    date_created = str(localtime(instance.project_overview.date_published).strftime('%Y-%m-%d_%I-%M_%p'))
+    return os.path.join(MEDIA_ROOT, instance.project_overview.project.project_code_name, date_created, "screenshots", filename)
 
 
 class ProjectOverviewScreenshot(models.Model):
     """ This object is a screenshot of the app, each ProjectAssets object can have multiple screenshots
     """
     project_overview = models.ForeignKey(ProjectOverview, related_name='screenshots')
-    screenshot = models.ImageField()
+    screenshot = models.ImageField(upload_to=overview_screenshot_upload_path)
 
     def screenshot_image(self):
         return mark_safe('<img src="%s" style="max-height: 100px; max-width: 100px;" />' % self.screenshot.url)
@@ -70,19 +84,32 @@ class Release(models.Model):
     is_archived = models.BooleanField(default=False)
     what_is_new = models.TextField()
     timestamp = models.DateTimeField(auto_now=True)
+    is_featured_release = models.BooleanField(default=False)
 
     def __str__(self):
         return '%d.%d.%d.%d' % (self.major_version, self.minor_version, self.point_version, self.build_version)
 
 
+def ipa_upload_path(instance, filename):
+    version_path = '%d.%d.%d.%d' % (instance.major_version, instance.minor_version, instance.point_version, instance.build_version)
+    platform_path = 'ios'
+    return os.path.join(MEDIA_ROOT, instance.ios_project.project_overview.project.project_code_name, platform_path, version_path, filename)
+
+
 class IosRelease(Release):
     ios_project = models.ForeignKey(IosProject, on_delete=models.CASCADE)
-    ipa_file = models.FileField()
+    ipa_file = models.FileField(upload_to=ipa_upload_path)
     manifest_file = models.FileField()
+
+
+def apk_upload_path(instance, filename):
+    version_path = '%d.%d.%d.%d' % (instance.major_version, instance.minor_version, instance.point_version, instance.build_version)
+    platform_path = 'android'
+    return os.path.join(MEDIA_ROOT, instance.android_project.project_overview.project.project_code_name, platform_path, version_path, filename)
 
 
 class AndroidRelease(Release):
     android_project = models.ForeignKey(AndroidProject, on_delete=models.CASCADE)
-    apk_file = models.FileField()
+    apk_file = models.FileField(upload_to=apk_upload_path)
 
 
