344921470029fd2cbb99f9d7da0f7b6214972380
Michael Ta <technical.ta@gmail.com>
2016-03-14 14:57:56 -0400

    creates manifests as temp files and puts them into the temp folder for later cleanup

--- applab/apps/create_manifest.py
+++ applab/apps/create_manifest.py
@@ -1,19 +1,24 @@
 from django.core.files import File
 from django.conf import settings
-from wsgiref.util import FileWrapper
-from django.shortcuts import HttpResponse, HttpResponseRedirect
-
-from django.core.files.temp import NamedTemporaryFile
+from django.shortcuts import HttpResponse
+import os, uuid
 
 def write_manifest_send(request, app, ipa_full_url):
 
+    tmp_directory =  'media/temp/'
     bundle_id = app.ios_project.bundle_id
     bundle_version = '{0}.{1}.{2}.{3}'.format(app.major_version,app.minor_version,app.point_version,app.build_version)
     app_title = app.ios_project.project_overview.project.title
     media_base_url = request.build_absolute_uri(settings.MEDIA_URL)
+    media_base_url = media_base_url.replace('/media/', '/')
     media_base_url = media_base_url.replace('http', 'https')
     ipa_full_url = ipa_full_url.replace('http', 'https')
-    file = open('media/manifest.plist', 'w')
+
+    
+    if not os.path.exists(tmp_directory):
+        os.makedirs(tmp_directory)
+    file_name = tmp_directory + str(uuid.uuid1())+'_manifest.plist'
+    file = open(file_name, 'w')
 
     file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     file.write('<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n')
@@ -60,11 +65,8 @@ def write_manifest_send(request, app, ipa_full_url):
     file.write('</plist>\n')
     file.close()
 
-    # new_manifest = open('media/manifest.plist')
 
-    # app.manifest_file.save('manifest.plist', File(new_manifest), save=False)
-    # wrapper = FileWrapper(file)
-    response = HttpResponse('', status=302)
-
-    response['Location'] = 'itms-services://?action=download-manifest&url='+media_base_url+'manifest.plist'
+    response = HttpResponse('', status=302, content_type='application/xml')
+    # response = HttpResponse(content_type='application/xml')
+    response['Location'] = 'itms-services://?action=download-manifest&url='+media_base_url+file_name
     return response
